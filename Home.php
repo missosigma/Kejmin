f46d106f27af7a297f063a660dbdad86eb60225c
missosigma <mmujgahn@gmail.com>
2026-07-30 12:48:18 -0400

    Rename images & add icon

--- Home.php
+++ Home.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kejmin Welcome Page</title>
+  <link rel= "icon" type = "image/x-icon" href = "K_Images/Leaf Slug.png">
 </head>
 <body>
     <h1>Welcome to the Kejmin starting page.</h1>
