43423cd916e90dee884d1f2788ffa6d4508ad5a2
Jaelyn Thoun <jaelynthoun095@gmail.com>
2026-08-03 13:31:11 -0400

    Added array for boundaries on index.js

--- index.js
+++ index.js
@@ -2,24 +2,48 @@ const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d');
 
-console.log(collisions);
-
 canvas.width = 1024;
 canvas.height = 576;
 
 const collisionsMap = [];
 for (let i = 0; i < collisions.length; i+= 60) {
-    collisionsMap.push(collisions.slice(i,i+60));
+    collisionsMap.push(collisions.slice(i, 60 + i));
     
 }
 
+console.log(collisionsMap);
+
 class Boundary {
+    static width = 48;
+    static height = 48;
     constructor(position) {
         this.position = position;
-        this.width
-        this.height
+        this.width = 48;
+        this.height = 48;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 }
+
+const boundaries = [];
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 554)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width,
+                    y: i * Boundary.height
+                }
+        }));
+    })
+})
+
+console.log(boundaries);
 
 const image = new Image();
 image.src = './K_Images/town.png';
