28199c1b76e28bd0238392d8cc8c88fe00d7b911
NatuNatu <natnat242027@gmail.com>
2026-08-03 13:43:22 -0400

    Removed redundant menu

--- KejMain.js
+++ KejMain.js
@@ -16,30 +16,10 @@ let spritex = canvas.width/2-spriteSize/2;
 let spritey = canvas.height/2-spriteSize/2/16*25;
 let direction;
 let spritesy = 0;
-let menuing = false;
-let menuIndex = 0;
-let menuSelect = false;
 
 function clear() {
     context.fillStyle = "rgb(255, 255, 255)";
     context.fillRect(0,0,canvas.width,canvas.height);
-}
-function clearMenu() {
-    context.fillStyle = "rgb(0, 0, 0)";
-    context.strokeRect(10,10,210,310);
-    context.fillStyle = "rgb(255, 255, 255)";
-    context.fillRect(10,10,210,310);
-}
-
-function drawMenu() {
-    context.fillStyle = "rgb(0, 0, 0)";
-    context.font = `36px Verdana`;
-    context.fillText("Kejmins",60,60,canvas.width/4-10);
-    context.fillText("Items",60,120,canvas.width/4-10);
-    context.fillText("Map",60,180,canvas.width/4-10);
-    context.fillText("",60,240,canvas.width/4-10);
-    context.fillText("Options",60,300,canvas.width/4-10);
-    context.beginPath(); context.moveTo(20,30+60*menuIndex); context.lineTo(20,60+60*menuIndex); context.lineTo(50,45+60*menuIndex); context.closePath(); context.fill();
 }
 
 function drawWorld() {
@@ -106,80 +86,27 @@ function drawWorld() {
 } 
 
 function gameCheck() {
-  if(!menuing) {
-    clear();
-    drawWorld();
-  }
-  if(menuing && !menuSelect) {
-    clearMenu();
-    drawMenu();
-  }
-  if(menuSelect) {
-    switch(menuIndex) {
-      case 0:
-        // Party
-        break;
-      case 1:
-        // Items
-        break;
-      case 2:
-        // Map
-        break;
-      case 3: 
-        // 
-        break;
-      case 4: 
-        // Options
-        break;
-    }
-  }
+  clear();
+  drawWorld();
 }
 
 window.setInterval(gameCheck,1000/16);
 
 document.addEventListener('keydown', (event) => {
-  if ((event.key === "x" || event.key === "c") && menuing && !menuSelect) {
-    menuing = false;
-  }
-  else if (event.key === "x" && menuSelect) {
-    menuSelect = false;
-    drawWorld();
-  }
-  else if (event.key === "z" && menuing && !menuSelect) {
-    menuSelect = true;
-    clear();
-  }
-  else if (event.key === "c") {
-    menuing = true;
-  }
   if (event.key === 's') {
-    if (!menuing) {
-        walking = true;
-        direction = "down";
-    }
-    if (menuing && menuIndex < 4) {
-        menuIndex++;
-    }
+    walking = true;
+    direction = "down";
   }
   else if (event.key === 'w') {
-    if(!menuing) {
-        walking = true;
-        direction = "up";
-    }
-    if (menuing && menuIndex > 0) {
-        menuIndex--;
-    }
+    walking = true;
+    direction = "up";
   }
   else if (event.key === 'a') {
-    if(!menuing) {
-        walking = true;
-        direction = "left";
-    }
+    walking = true;
+    direction = "left";
   }
   else if (event.key === 'd') {
-    if(!menuing) {
-        walking = true;
-        direction = "right";
-    }
+    walking = true;
+    direction = "right";
   }
 });
