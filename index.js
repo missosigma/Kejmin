c0f21750a935b88cfa64afd09936fef825378e5d
Jaelyn Thoun <jaelynthoun095@gmail.com>
2026-08-03 13:14:01 -0400

    Updated index.html and index.js

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ console.log(collisions);
 
 canvas.width = 1024;
 canvas.height = 576;
-console.log(canvas);
 
 const collisionsMap = [];
-for (let i = 0; i < collisions.length; i+= 70) {
-    collisionsMap.push(collisions.slice(i,i+70));
+for (let i = 0; i < collisions.length; i+= 60) {
+    collisionsMap.push(collisions.slice(i,i+60));
     
 }
 
@@ -24,7 +23,6 @@ class Boundary {
 
 const image = new Image();
 image.src = './K_Images/town.png';
-console.log(image);
 
 image.onload = () => {
     c.drawImage(image, -100, -400);
