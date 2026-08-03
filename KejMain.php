5f829554588d91d1af1fa00c49231919802e7f24
missosigma <mmujgahn@gmail.com>
2026-08-03 13:52:18 -0400

    session start on pages with navbar

--- KejMain.php
+++ KejMain.php
@@ -1,4 +1,5 @@
 <html lang="en">
+    <?php session_start();?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,6 +7,7 @@
     <link rel= "icon" type = "image/x-icon" href = "K_Images/KJMN.png">
 
 </head>
+
 <body>
     <?php
         include "navbar.htm";
--- kejmin.php
+++ kejmin.php
@@ -1,4 +1,5 @@
 <link rel= "icon" type = "image/x-icon" href = "K_Images/KJMN.png">
+<?php session_start();?>
 <?php 
         include "navbar.htm";
     ?> 
