4a8c71a1876b1475cf0f7fa96d68656dcdc325f0
Jaelyn Thoun <jaelynthoun095@gmail.com>
2026-08-04 09:40:03 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -48,6 +48,17 @@ const foreground = new Sprite({
     image: foregroundImage
 })
 
+const movables = [background, ...boundaries, foreground];
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+    );
+}
+
 function animate() {
     window.requestAnimationFrame(animate);
     background.draw();
