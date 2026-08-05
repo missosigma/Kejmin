c63c959684bd5effd88e6d1ab7c8c37955c89a2c
Kenzie <beck1@etown.edu>
2026-08-05 11:29:18 -0400

    Added an attack move bad

--- Battle Navbar Files/attack.php
+++ Battle Navbar Files/attack.php
@@ -5,3 +5,52 @@ What we want!! !!
 and the opponent's health lowers.
 
 - (xtra - any effects)
+<?php include "../battlenavbar.php" ?>
+<html>
+    <div class ="bar">
+<nav class = "bottom-right-nava">
+<ul>
+<li> Move 1: </li>
+<li> Move 2: </li>
+<li> Move 3: </li>
+</ul>
+</nav>
+</div>
+</html>
+<style>
+ 
+    .bottom-right-nava{
+    position:fixed;
+    bottom:100px;
+    right:100px;
+    z-index:1000;
+    border: 1px solid black;
+    background-color: black;
+    padding: 2px;
+  
+} 
+
+ul{
+      list-style-type:none;
+            margin:0;
+            padding:0;
+            overflow:hidden;
+            background-color:#B9A5E2;
+            font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;                
+}
+
+.bottom-right-nava li{
+    display:block;
+            color:white;
+            padding:14px 26px;
+            text-decoration:none;
+            font-size:20px;
+            margin-top:13px;
+            -webkit-text-stroke:1px black;
+            
+}
+
+  .bottom-right-nava li:hover{
+            background-color: #a898ce;
+        }
+  </style>
