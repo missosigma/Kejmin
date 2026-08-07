6fb38ab1d994ed08b92cbd9f8444cf3558dd249a
Kenzie <beck1@etown.edu>
2026-08-07 09:56:18 -0400

    Removed underline for credit button

--- about.php
+++ about.php
@@ -37,12 +37,15 @@ p{
     font-weight:bold;
     margin: 1px 1px 1px 1px;
 }
+button, .button-class, a {
+    text-decoration: none; 
+}
   .button{
     padding: 10px 20px;
     font-size: 16px;
     color: rgb(200, 251, 255);
     border-radius: 15px;
-    font-weight: bold;
+
     font-style: italic;
     background-color: rgb(235, 59, 212);
     border: 2px solid rgb(123, 12, 148);
